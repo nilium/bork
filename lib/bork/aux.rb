069865b9666ef5f95b09dfa247da136da8f82cbf
Noel Cower <me@spifftastic.net>
2012-07-20 18:03:00 -0700

    Add extract_file_tag_arguments to bork/aux.

--- lib/bork/aux.rb
+++ lib/bork/aux.rb
@@ -33,6 +33,9 @@ module Bork
   # used by version
   @@DOT_DIR_REGEX = /^\.{1,2}$/
 
+  # used by extract_file_tag_arguments
+  @@PRIVATE_FILE_REGEX = %r((^|/)\.(?!=$|/))
+
   def self.dot_dir_regex
     @@DOT_DIR_REGEX
   end
@@ -56,6 +59,55 @@ module Bork
     end
 
     target_path.relative_path_from(source_path).to_s
+  end
+
+  # Extracts files and tags from the arguments and returns arrays of both.
+  def self.extract_file_tag_arguments args
+    tags = []
+    files = []
+
+    # can be :any, :tag, or :tags
+    # :any -> check the argument. If not a valid option, it's a file.
+    # :tag -> this argument is a tag.
+    # :tags -> all arguments hereinafter are tags.
+    arg_state = :any
+    args.each {
+      |arg|
+      case arg_state
+      when :any
+        case arg
+        when '-t', '--tag' then arg_state = :tag
+        when '-T', '--tags' then arg_state = :tags
+        else files << arg
+        end
+      when :tag
+        tags << arg
+        arg_state = :any
+      when :tags
+        tags << arg
+      end
+    }
+
+    files = files.reduce([]) {
+      |results, file_path|
+
+      if File.exists? file_path
+        if File.directory? file_path
+          results += Dir.glob("#{file_path}/**/*").reject {
+            |entry|
+            entry =~ @@PRIVATE_FILE_REGEX || File.directory?(entry)
+          }
+        else
+          results << file_path
+        end
+      else
+        raise "No such file '#{file_path}'."
+      end
+
+      results
+    }
+
+    return files, tags
   end
 
 end
